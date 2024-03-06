3698fe4bb50e14c59acffe95cd4b76864e321eda
GashemaKalisa <147123309+GashemaKalisa@users.noreply.github.com>
2024-03-06 13:57:47 -0500

    Add project files.

---
+++ Students_Data/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Student_DatabaseEntities();
+            var post = new Students_Table
+            {
+                Students_Id=001,
+                FirstName = "Gashema",
+                LastName ="Kalisa",
+                Grade="A"
+            };
+            context.Students_Table.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
